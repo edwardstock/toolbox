36a761ccbcf0e1a86f501bcd85be1778ed5423c1
Eduard Maximovich <edward.vstock@gmail.com>
2017-12-18 14:45:22 +0300

    trying to sudo inside script (i hate you travis)

--- llvm_config.sh
+++ llvm_config.sh
@@ -2,8 +2,8 @@
 
 if [ hash `llvm-config 2>/dev/null` ] && [ ! -f /etc/ld.so.conf.d/llvm.conf ];
 then
-	echo `llvm-config --libdir` >> /etc/ld.so.conf.d/llvm.conf
-	echo `ldconfig`
+	sudo echo `llvm-config --libdir` >> /etc/ld.so.conf.d/llvm.conf
+	sudo ldconfig
 	echo "Added llvm lib dir to ldconfig"
 	export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:`llvm-config --libdir`
 fi
