3bf8d673bcd5918798559bd0381e855cc6665abb
Eduard Maximovich <edward.vstock@gmail.com>
2019-05-18 03:05:59 +0300

    ci

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -2,7 +2,8 @@
 set -e
 
 conan create . scatter/testing
-conan export . toolboxpp/2.0.0@scatter/testing
-conan test . toolboxpp/2.0.0@scatter/testing
+conan export . toolboxpp/2.1.0@scatter/testing
+conan test . toolboxpp/2.1.0@scatter/testing
 conan create . scatter/latest
-conan upload toolboxpp/2.0.0@scatter/latest --all -r=scatter
+conan export-pkg . toolboxpp/2.1.0@scatter/testing
+conan upload toolboxpp/2.1.0@scatter/latest --all -r=scatter
