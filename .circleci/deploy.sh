c9de63ba047807782ea2c6d006e64d840f983ad5
Eduard Maximovich <edward.vstock@gmail.com>
2019-05-18 03:09:55 +0300

    ci again

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-conan create . scatter/testing
-conan export . toolboxpp/2.1.0@scatter/testing
-conan test . toolboxpp/2.1.0@scatter/testing
 conan create . scatter/latest
-conan export-pkg . toolboxpp/2.1.0@scatter/testing
+conan export-pkg . toolboxpp/2.1.0@scatter/latest
 conan upload toolboxpp/2.1.0@scatter/latest --all -r=scatter
