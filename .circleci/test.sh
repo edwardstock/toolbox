f0e810c684e8721d348d32f72fcab19d89beade6
Eduard Maximovich <edward.vstock@gmail.com>
2019-12-07 04:52:01 +0300

    try to disable coverage

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -3,6 +3,6 @@
 set -e
 
 mkdir _build && cd _build
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DENABLE_TOOLBOX_TEST=On -DWITH_COVERAGE=On
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DENABLE_TOOLBOX_TEST=On -DWITH_COVERAGE=Off
 cmake --build . --target toolbox-test
 ./toolbox-test
